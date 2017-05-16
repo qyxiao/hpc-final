652818bdece89859601d73cde927672d91f89e9f
qyxiao <qx344@nyu.edu>
2017-05-16 11:10:33 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hpc-final
+
+mpicc -I/home/qx344/qx344/fftwmpi/install/include  main_mpi.c  -lfftw3_mpi -lfftw3 -lm -g -fopenmp  -L/home/qx344/qx344/fftwmpi/install/lib -o test3
--- mpiff2.c
+++ mpiff2.c
@@ -1,51 +1,50 @@
 #include <stdio.h> 
+#include<stdio.h>
+#include<stdlib.h>
 #include <stddef.h> 
 #include <stdint.h>
+#include "util.h"
 #include <complex.h>
 #include <fftw3-mpi.h>
 
 int main(int argc, char **argv)
 {
 
-    int Npoints = 16, mpirank;
-    ptrdiff_t N0 = 4, N1 = 4;
+    int iteration=0, maxIter=10, mpirank;
+    int Npoints = 4;
+    if(argc>=2){sscanf(argv[1], "%d", &Npoints);}  
+    if(argc>=3){sscanf(argv[2], "%d", &maxIter);} 
     fftw_plan plan;
-    fftw_complex *data;
+    fftw_complex *data, *datahat;
     ptrdiff_t alloc_local, local_n0, local_0_start, i, j;
+    
 
     MPI_Init(&argc, &argv);
     fftw_mpi_init();
     MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
     /* get local data size and allocate */
-    alloc_local = fftw_mpi_local_size_2d(N0, N1, MPI_COMM_WORLD,
+    alloc_local = fftw_mpi_local_size_2d(Npoints, Npoints, MPI_COMM_WORLD,
                                          &local_n0, &local_0_start);
     data = fftw_alloc_complex(alloc_local);
-
+    datahat = fftw_alloc_complex(alloc_local);
     /* create plan for in-place forward DFT */
-    plan = fftw_mpi_plan_dft_2d(N0, N1, data, data, MPI_COMM_WORLD,
-                                FFTW_FORWARD, FFTW_ESTIMATE);    
-
+    plan = fftw_mpi_plan_dft_2d(Npoints, Npoints, data, datahat, MPI_COMM_WORLD,
+                                FFTW_FORWARD, FFTW_ESTIMATE);     
     /* initialize data to some function my_function(x,y) */
-    int iteration = 0;
     fftw_complex iter = 0;
-    while(iteration<2){
-        if(iteration>0){iter = mpirank*N1;}
+
+    iter = mpirank*Npoints;
     for (i = 0; i < local_n0; ++i){
-        for (j = 0; j < N1; ++j){
-            data[i*N1 + j] = iter;
-            if(iteration>0){iter ++;}
+        for (j = 0; j < Npoints; ++j){
+            data[i*Npoints + j] = iter;
+            iter ++;
         }
     } 
-        if(iteration==0){data = fftw_alloc_complex(alloc_local);}
-        
+
+    while(iteration<maxIter){
+        fftw_execute(plan);
         iteration++;
     }
-    
-    printf("rank %d: %d = %d?\n",mpirank,mpirank*N1,local_0_start);
-    fftw_destroy_plan(plan);       
-    plan = fftw_mpi_plan_dft_2d(N0, N1, data, data, MPI_COMM_WORLD,FFTW_FORWARD, FFTW_ESTIMATE); 
-    /* compute transforms, in-place, as many times as desired */
-    fftw_execute(plan);
     
 
     FILE* fd = NULL;
@@ -53,10 +52,10 @@ int main(int argc, char **argv)
     snprintf(filename, 256, "outputTest%d.txt",mpirank);
     fd = fopen(filename,"w+");
     iter = 0;
-    for(i = 0; i < local_n0*N1; i++)
+    for(i = 0; i < local_n0*Npoints; i++)
     {
         //fprintf(fd, "%d\n", i);
-        fprintf(fd, "%f %11.7f %11.7f\n", creal(iter), creal(data[i]), cimag(data[i]));
+        fprintf(fd, "%td %11.7f %11.7f\n", i, creal(datahat[i]), cimag(datahat[i]));
         //fprintf(fd, "%d %f \n", i, vorticity[i]);
         iter = iter+1;
     }
@@ -64,4 +63,5 @@ int main(int argc, char **argv)
     fftw_destroy_plan(plan);
 
     MPI_Finalize();
+    return 0;
 }
